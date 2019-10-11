8973c2fad24ed2e396140698b6b9d89206636d86
dingsheng <343776195@qq.com>
2019-10-11 13:46:28 +0800

    供应商数据同步-同步数据到MySQL

---
+++ 参数配置/数据集成/S组/供应商数据同步/同步数据到MySQL.sh
@@ -0,0 +1,18 @@
+ -- 供应商数据同步 
+ -- 同步数据到MySQL 
+ CREATE TABLE `test_user_behavior_2` (
+ `action` string,
+ `bhv_count` string,
+ `client_id` string,
+ `content_id` string,
+ `content_type` string,
+ `created` string,
+ `descri` string,
+ `env` string,
+ `page_id` string,
+ `trace_id` string,
+ `user_id` string,
+  `value` string
+ ) 
+  PARTITIONED BY (ds string)
+  LIFECYCLE 100000;
